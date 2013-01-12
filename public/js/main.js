4aedff3278092546bad083d54913747e04c4704f
Valentin Tsatskin <val@valtsatskin.com>
2013-01-11 22:27:52 -0500

    login button hook

---
+++ public/js/main.js
@@ -0,0 +1,23 @@
+// Load the SDK Asynchronously
+(function(d){
+   var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+   if (d.getElementById(id)) {return;}
+   js = d.createElement('script'); js.id = id; js.async = true;
+   js.src = "//connect.facebook.net/en_US/all.js";
+   ref.parentNode.insertBefore(js, ref);
+ }(document));
+
+
+function login() {
+  FB.login(function(response) {
+      if (response.authResponse) {
+          // connected
+      } else {
+          // cancelled
+      }
+  });
+}
+
+$(function(){
+  $("#login-button").click(login);
+});
