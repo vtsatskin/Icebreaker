975b95f8c7238e1c7ad2601550e9af1f397de0b0
Valentin Tsatskin <val@valtsatskin.com>
2013-01-12 06:12:04 -0500

    get_results changes

--- models/user.rb
+++ models/user.rb
@@ -17,7 +17,6 @@ class User
   property :created_at,             DateTime
   property :updated_at,             DateTime
   has 1, :room, :through => Resource
-  has n, :users, self, :through => :room
   has n, :likes, :through => Resource
   has n, :educations
   has n, :games
@@ -34,61 +33,64 @@ class User
     self.save
   end
 
+  def get_room_likes
+  end
+
   def get_results
-    results = Array.new
-    each self.users do |match|
-      Int score = 0
-      if self.gender != match.gender and self.single == true and match.single == true
-        score += 20
-      end
-      if self.hometown == match.hometown
-        score += 15
-      end
-      if self.current_city == match.current_city
-        score += 5
-      end
-      if self.birthday == match.birthday
-        score += 5
-      end
-      each self.likes do |like|
-        if match.likes.include?(like)
+    if room = self.room
+      (room.users - self).map do |match|
+        score = 0
+        if self.gender != match.gender and self.single == true and match.single == true
+          score += 20
+        end
+        if self.hometown == match.hometown
+          score += 15
+        end
+        if self.current_city == match.current_city
           score += 5
         end
-      end
-      each self.educations do |education|
-        if match.educations.include?(education)
-          score += 10
+        if self.birthday == match.birthday
+          score += 5
         end
-      end
-      each self.games do |game|
-        if match.games.include?(game)
-          score += 2
+
+        mutual_likes = self.likes & match.likes
+        score += 5 * mutual_likes.count
+
+        self.educations.each do |education|
+          if match.educations.include?(education)
+            score += 10
+          end
         end
-      end
-      each self.events do |event|
-        if match.events.include?(event)
-          score += 3
+        self.games.each do |game|
+          if match.games.include?(game)
+            score += 2
+          end
         end
-      end
-      each self.beliefs do |belief|
-        if match.beliefs.include?(belief)
-          score += 10
+        self.events.each do |event|
+          if match.events.include?(event)
+            score += 3
+          end
         end
-      end
-      each self.interests do |interest|
-        if match.interests.include?(interest)
-          score += 7
+        self.beliefs.each do |belief|
+          if match.beliefs.include?(belief)
+            score += 10
+          end
         end
-      end
-      each self.workhistorys do |workhistory|
-        if match.workhistorys.incude?(workhistory)
-          score += 10
+        self.interests.each do |interest|
+          if match.interests.include?(interest)
+            score += 7
+          end
         end
+        self.workhistorys.each do |workhistory|
+          if match.workhistorys.incude?(workhistory)
+            score += 10
+          end
+        end
+        
+        { :score => score, :user => match, :mutual_likes => mutual_likes }
       end
-      hash = { :score => score, :user => match }
-      array.push
+    else
+      []
     end
-    results.sort_by { |usr| usr[:score] }
-    return results
   end
 end
